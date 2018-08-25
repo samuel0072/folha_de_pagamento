2229c6e84841d63c80bd6af498f0fcf5dc9cf59e
Samuel <30638348+samuel0072@users.noreply.github.com>
2018-08-25 10:28:20 -0300

    Add files via upload

--- src/empregado/agenda/Agenda.java
+++ src/empregado/agenda/Agenda.java
@@ -2,8 +2,14 @@ package empregado.agenda;
 
 public class Agenda {
     private String nome;
+    private String tipo;
+    int duracao;
+    String dia;
 
-    public Agenda(String nome_){
-        this.nome = nome_;
+    public Agenda(String tipo_, String dia_,  int duracao_){
+        this.nome = "Semanal "+ dia_ + " "+ tipo_;
+        this.duracao = duracao_;
+        this.dia = dia;
+        this.tipo = tipo_;
     }
 }
